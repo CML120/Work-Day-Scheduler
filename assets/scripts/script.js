27da4d617f5ffb2972c519f6f4abc6cb2581025d
CML120 <cheelor922@gmail.com>
2023-04-09 15:42:38 -0500

    added comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,13 +1,18 @@
+//will run once the page DOM is ready for script code
 $(document).ready(function () {
 
+
+    //dayjs code to get day, date, time
     setInterval(function () {
         $("#day").text(dayjs().format("dddd[, ]"));
         $('#date').text(dayjs().format('MMM DD, YYYY'));
         $("#time").text(dayjs().format("hh:mm:ss a"));
     }, 1000)
 
+    //variable for the hour, will be used to compare time block's time
     var currentHour = dayjs().format("HH");
 
+    //check time blocks and set them to past, present, or future class to determine time block color for display purposes
     $(".time-block").each(function () {
         var timeBlock = $(this).attr('id').split("-")[1];;
         if (currentHour == timeBlock) {
@@ -27,6 +32,7 @@ $(document).ready(function () {
         }
     })
 
+    // saves user input in the schedule to localstorage
     $(".saveBtn").click(function (event) {
         event.preventDefault();
         var strInput = $(this).siblings(".description").val();
@@ -35,11 +41,13 @@ $(document).ready(function () {
         localStorage.setItem(time, strInput);
     });
 
+    //clears user input from the schedule by clearing localstorage
     $("#resetBtn").on("click", function () {
         localStorage.clear();
         getSchedule();
     })
 
+    //displays information into the schedule if it exists in local storage
     function getSchedule() {
         $("#hour-09 .description").val(localStorage.getItem("hour-09"));
         $("#hour-10 .description").val(localStorage.getItem("hour-10"));
@@ -51,5 +59,7 @@ $(document).ready(function () {
         $("#hour-16 .description").val(localStorage.getItem("hour-16"));
         $("#hour-17 .description").val(localStorage.getItem("hour-17"));
     }
+
+    //calls the function to execute
     getSchedule();
 });
